27a1752ecb9eb25692645109868275fafd357e13
Ondine Debienne <ondine.debienne@gmail.com>
2023-08-01 14:15:10 +0200

    HomePage component w navbar

---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="main-image">
+      <div class="card">
+        <h1></h1>
+      </div>
+    </div>
+    <ProductsList></ProductsList>
+  </div>
+</template>
+
+<script>
+import ProductsList from "../components/ProductsList.vue";
+
+export default {
+  components: {
+    ProductsList,
+  },
+  data() {},
+};
+</script>
+
+<style>
+.main-image {
+  background-image: url("../assets/home-main-image.jpg");
+  background-size: cover;
+  background-position: center;
+  height: 400px;
+}
+</style>
