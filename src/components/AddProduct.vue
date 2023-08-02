7f9eba9110bdc08d1e59b1d5d4d97d294202c84c
thenriet <dabalx@gmail.com>
2023-08-02 15:31:56 +0200

    form for addProduct without picture

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,10 +1,11 @@
 <template lang="">
   <div class="container mt-3 w-50">
     <h1>Créer un nouveau produit</h1>
-    <form action="">
+    <form action="" v-on:submit.prevent>
       <div class="mb-3">
         <label for="FormControlTitle" class="form-label">Nom du produit</label>
         <input
+          v-model="formTitle"
           type="text"
           class="form-control"
           id="title"
@@ -16,6 +17,8 @@
           >Description du produit</label
         >
         <textarea
+          style="white-space: pre-line"
+          v-model="formDescription"
           class="form-control"
           id="description"
           rows="3"
@@ -25,7 +28,8 @@
       <div class="mb-3">
         <label for="FormControlPrice" class="form-label">Prix du produit</label>
         <input
-          type="text"
+          v-model.number="formPrice"
+          type="number"
           class="form-control"
           id="price"
           placeholder="Prix du produit"
@@ -36,7 +40,8 @@
           >Poids du produit</label
         >
         <input
-          type="text"
+          v-model.number="formWeight"
+          type="number"
           class="form-control"
           id="weight"
           placeholder="Poids du produit"
@@ -46,17 +51,73 @@
         <label for="FormControlCategory" class="form-label"
           >Catégorie du produit</label
         >
-        <input type="text" class="form-control" id="category" />
+        <select
+          v-model="selectedCategory"
+          name="categories"
+          id="FormControlCategory"
+          class="form-control"
+        >
+          <option value="">Choisir une catégorie</option>
+          <option value="toys">Jouet</option>
+          <option value="food">Nourriture</option>
+          <option value="accessories">Accessoires</option>
+          <option value="utilities">Utilitaires</option>
+        </select>
       </div>
       <div class="mb-3">
         <label for="FormControlPicture" class="form-label">Image</label>
         <input type="text" class="form-control" id="image" />
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button @click="createJson" type="submit" class="btn btn-primary">
+        Submit
+      </button>
     </form>
+    <div class="test">
+      <ul>
+        <li>{{ formTitle }}</li>
+        <li>{{ formDescription }}</li>
+        <li>{{ formPrice }}</li>
+        <li>{{ formWeight }}</li>
+        <li>{{ selectedCategory }}</li>
+      </ul>
+    </div>
   </div>
 </template>
+
 <script>
-export default {};
+import ProductsDataService from "../services/ProductsDataService";
+
+export default {
+  name: "AddProduct",
+  data() {
+    return {
+      formTitle: "",
+      formDescription: "",
+      formPrice: null,
+      formWeight: null,
+      selectedCategory: null,
+    };
+  },
+  methods: {
+    createJson() {
+      let data = {
+        title: this.formTitle,
+        description: this.formDescription,
+        price: this.formPrice,
+        weight: this.formWeight,
+        category: this.selectedCategory,
+      };
+      this.createProduct(data);
+    },
+    createProduct: async function (data) {
+      try {
+        await ProductsDataService.create(data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
 </script>
+
 <style></style>
